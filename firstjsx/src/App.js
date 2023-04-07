5b064719b817ff5b49e9197d9e76cd8f6607993e
Christian Johnson <charcoaldust@mac.com>
2023-04-07 16:53:46 -0700

    initialized 'first jsx' project

---
+++ firstjsx/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="content">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+        <li>Other CRUD</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
